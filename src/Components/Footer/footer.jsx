c6172532d40752ecfd6827e702525b22d998094d
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2023-09-01 19:59:21 +0100

    Footer Updated

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -5,7 +5,7 @@ function Footer(){
     return(
         <div>
             <div className="footerDetailer">
-                <p>Made it by:</p>
+                <p>Made by:</p>
                 <Link to="https://github.com/Aamir269">Aamir Mushtaq</Link>
                 <Link to="https://github.com/Vayre047">Tomás Borda de Água</Link>
             </div>
