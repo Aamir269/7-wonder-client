30e818348eabb479b3ce0f76ba00d9d72e1084fc
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2023-09-02 14:49:17 +0100

    Collecting some images

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,35 +1,33 @@
-import { Link } from 'react-router-dom';
-import { useContext } from 'react';
-import { AuthContext } from '../../Context/context.auth';
+import {Link} from 'react-router-dom';
+import {useContext} from 'react'; 
+import { AuthContext } from '../../Context/auth.context';
 
-function Navbar(){
-    const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+function Navbar() {
+  const {isLoggedIn, user, logOutUser} = useContext(AuthContext);
 
-    return(
-        <div>
-            <div>
-                <Link to="/">
-                    <button>Home</button>
-                </Link>
-            </div>
-            <div>
-                <h2>Welcome to 7 Wonders of the World Explorer!</h2>
-            </div>
-            {isLoggedIn ? (
-                    <div>
-                        <button onClick={logOutUser}>Logout</button>
-                        <p>{user && user.name}</p>
-                    </div>
-                ) :
-                (
-                    <div>
-                        <Link to="/signup"><button>Signup</button></Link>
-                        <Link to="/login"><button>Login</button></Link>
-                    </div>
-                )
-            }
-        </div>
-    );
+
+  return (
+    <nav>
+    <div>
+    <Link to="/">
+        <button>Home</button>
+    </Link>
+    </div>
+    {isLoggedIn? (
+      <div>
+        <button onClick={logOutUser}>Logout</button>
+        <p>{user && user.name}</p>
+      </div>
+    ): 
+    (
+      <div>
+        <Link to="/signup"><button>Signup</button></Link>
+        <Link to="/login"><button>Login</button></Link>
+      </div>
+    )
+  } 
+    </nav>
+  )
 }
 
 export default Navbar;
--- src/Pages/HomePage/homepage.jsx
+++
@@ -1,9 +0,0 @@
-function Homepage(){
-    return(
-        <div>
-            <p>This web application allows you to explore and learn about historical places around the world.</p>
-        </div>
-    );
-} 
-
-export default Homepage; 
