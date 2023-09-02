fdb093b6df4d2ec7543b29caeeb47c03e6843eda
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2023-09-02 15:22:22 +0100

    Delete the delete component

--- src/Pages/WonderDetail/wonderDetail.jsx
+++ src/Pages/WonderDetail/wonderDetail.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import UpdateReview from "../UpdateReview/updateReview";
-import DeleteReview from "../DeleteReview/deleteReview";
 import CreateReview from "../CreateReview/createReview";
 
 const API_URL = "http://localhost:5005";
