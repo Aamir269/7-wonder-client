daf2da1c11da7d361959b2fd31d1e2db01096d72
Aamir269 <mushtaqaamir047@gmail.com>
2023-09-07 10:05:11 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import UpdateReview from './Pages/UpdateReview/updateReview';
 import LogIn from './Pages/Login/login';
 import WonderDetail from './Pages/WonderDetail/wonderDetail';
 import CreateReview from './Pages/CreateReview/createReview';
+import MainTitle from './Components/MainTitle';
 import EditReview from './Pages/EditReview/editReview'; // Add/EditReview import
 import { Route, Routes } from 'react-router-dom';
 
@@ -24,6 +25,7 @@ const App = () => {
         <Route path='/wonder/:wonderId' element={<WonderDetail />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/login' element={<LogIn />} />
+        <Route path='/mainTitle' element={<MainTitle />} />
         <Route path='/wonder/:wonderId/edit/:reviewId' element={<IsPrivate><EditReview /></IsPrivate>} /> {/* Add/EditReview route */}
       </Routes>
       <Footer />
--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,5 +1,6 @@
+// Navbar.jsx
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
 import { AuthContext } from "../../Context/context.auth";
 import WonderCard from "../../Pages/WonderCard/wonderCard";
 import './navbar.css';
@@ -8,34 +9,36 @@ function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   return (
-    <div className="navbarContainer gap-x-4 h-10 mt-8 mb-8">
-      <div className="mt-3">
-        <Link to="/">
-          <button className="navbarBtnConfiguration border border-blue-700 rounded bg-blue-700 hover:bg-blue-950 hover:border-blue-950 text-neutral-50 me-12">Home</button>
-        </Link>
+    <div className="header">
+      <div className="navbarContainer">
+        <div className="navbarLeft">
+          <Link to="/">
+            <button className="navbarBtn Configuration border border-blue-700 rounded bg-blue-700 hover:bg-blue-950 hover:border-blue-950 text-neutral-50 me-12">Home</button>
+          </Link>
+        </div>
+        <div className="navbarRight">
+          {isLoggedIn ? (
+            <div className="flex gap-x-4">
+              <Link to="/wonder/card" element={<WonderCard />} className="navbarWonderListBtn Configuration border border-blue-700 rounded bg-blue-700 hover:bg-blue-950 hover:border-blue-950 text-neutral-50">
+                <p className="mt-2.5">Wonder List</p>
+              </Link>
+              <p>{user && user.name}</p>
+              <button className="navbarBtn Configuration border border-blue-700 rounded bg-blue-700 hover:bg-blue-950 hover:border-blue-950 text-neutral-50" onClick={logOutUser}>Logout</button>
+            </div>
+          ) : (
+            <div className="flex gap-x-4">
+              <Link to="/signup">
+                <button className="navbarBtn Configuration border border-blue-700 rounded bg-blue-700 hover:bg-blue-950 hover:border-blue-950 text-neutral-50">Signup</button>
+              </Link>
+              <Link to="/login">
+                <button className="navbarBtn Configuration border border-blue-700 rounded bg-blue-700 hover:bg-blue-950 hover:border-blue-950 text-neutral-50">Login</button>
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
       <div className="mainTitle">
         <h2>Welcome to 7 Wonders of the World Explorer!</h2>
-      </div>
-      <div className="gap-x-4">
-        {isLoggedIn ? (
-          <div className="flex gap-x-4 mt-3">
-            <Link to="/wonder/card" element={<WonderCard />} className="navbarWonderListBtnConfiguration border border-blue-700 rounded bg-blue-700 hover:bg-blue-950 hover:border-blue-950 text-neutral-50">
-              <p className="mt-2.5">Wonder List</p>
-            </Link>
-            <p>{user && user.name}</p>
-            <button className="navbarBtnConfiguration border border-blue-700 rounded bg-blue-700 hover:bg-blue-950 hover:border-blue-950 text-neutral-50" onClick={logOutUser}>Logout</button>
-          </div>
-        ) : (
-          <div className="mt-3">
-            <Link to="/signup">
-                <button className="navbarBtnConfiguration border border-blue-700 rounded bg-blue-700 hover:bg-blue-950 hover:border-blue-950 text-neutral-50">Signup</button>
-            </Link>
-            <Link to="/login">
-                <button className="navbarBtnConfiguration border border-blue-700 rounded bg-blue-700 hover:bg-blue-950 hover:border-blue-950 text-neutral-50 ml-2.5">Login</button>
-            </Link>
-          </div>
-        )}
       </div>
     </div>
   );
